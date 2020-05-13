2ba6e1cef4a24204ed1087e79a51919f9a990f8e
ejKang <joomn11@gmail.com>
2020-05-13 14:29:12 +0900

    add project setup

---
+++ shoppingbackend/src/test/java/net/ejkang/shoppingbackend/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ejkang.shoppingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
