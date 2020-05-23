ee0c8c56c5a420bf4ccd7efd82e8614b8556a324
ejKang <joomn11@gmail.com>
2020-05-23 19:24:16 +0900

    sidebar active class add

---
+++ onlineshopping/src/main/resources/static/jquery/myapp.js
@@ -0,0 +1,7 @@
+var sidebarActiveMenu = function (id) {
+    if (document.getElementById('a_'+menu) != null) {
+        document.getElementById('a_'+menu).classList.add("active");
+    }
+}
+
+(function() {})
